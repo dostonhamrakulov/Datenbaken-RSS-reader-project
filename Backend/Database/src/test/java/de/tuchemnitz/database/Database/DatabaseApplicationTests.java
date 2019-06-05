521aad19c21a5ff58584dd5233bb4ea7bdd7bbeb
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-06-05 16:39:05 +0200

    base project

---
+++ Backend/Database/src/test/java/de/tuchemnitz/database/Database/DatabaseApplicationTests.java
@@ -0,0 +1,16 @@
+package de.tuchemnitz.database.Database;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatabaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
