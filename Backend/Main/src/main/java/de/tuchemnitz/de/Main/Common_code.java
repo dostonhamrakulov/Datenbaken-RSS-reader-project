d9258bc1733b3877d09c807a35385284cc148dbc
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-06-11 03:45:09 +0200

    date_time methods

--- Backend/Main/src/main/java/de/tuchemnitz/de/Main/Common_code.java
+++ Backend/Main/src/main/java/de/tuchemnitz/de/Main/Common_code.java
@@ -1,6 +1,8 @@
 package de.tuchemnitz.de.Main;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Common_code {
@@ -13,10 +15,33 @@ public class Common_code {
         Date d = new Date();
         return sdf.format(d);
     }
+    public static Date getCurrentDateinDate() throws ParseException {
+        sdf = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
+        return sdf.parse(getCurrentDate());
+    }
 
     public static String convertDateToString(Date date){
         sdf = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
         Date d = date;
         return sdf.format(d);
     }
+    public static Date convertStringToDate(String date) throws Exception{
+        sdf = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
+        return sdf.parse(date);
+    }
+    public static Date addDays(Date date, int days)
+    {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DAY_OF_MONTH, days);
+        return cal.getTime();
+    }
+
+//    public static String compareDates(String date1, String date2) throws Exception {
+//        sdf = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
+//        Date d1 = convertStringToDate(date1);
+//        Date d2 = convertStringToDate(date2);
+//
+//        if(d1.after())
+//    }
 }
